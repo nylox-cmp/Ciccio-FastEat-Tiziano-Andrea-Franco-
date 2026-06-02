dce36f2854b48e133bd25c324f28f8618f519609
nylox-cmp <tiziano88ita@gmail.com>
2026-06-02 18:52:14 +0200

    aggiunga del metodo set a windowError

--- src/main/java/gui/WindowError.java
+++ src/main/java/gui/WindowError.java
@@ -5,4 +5,8 @@ import javax.swing.*;
 public class WindowError {
     private JPanel MainPanel;
     private JLabel ErrorLabel;
+
+    public void set_error_label_text(String text){
+        ErrorLabel.setText(text);
+    }
 }
